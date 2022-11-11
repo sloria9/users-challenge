9625a9540053d7c0285d957486a6231308dfe513
sofia.loria <sofia.loria@globallogic.com>
2022-11-10 23:58:39 -0300

    password encoder

---
+++ src/main/java/com/challenge/users/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.challenge.users.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+		http.authorizeRequests()
+		.antMatchers("/challenge/v1/login").permitAll()
+		.antMatchers("/challenge/v1/sign_up").permitAll()
+		.and()
+		.httpBasic();
+	}
+
+}
